fd508eb74bc45d9962fcc88f99ddc247aa7d353c
Andreas Johansson <thep00dl3@gmail.com>
2012-04-10 14:24:19 +0200

    Gjorde FETT mycket bra saker!! Som typ slick

--- Lauzer-Wars/src/testPackage/Main.java
+++ Lauzer-Wars/src/testPackage/Main.java
@@ -6,20 +6,16 @@ import java.awt.Color;
 import javax.swing.JFrame;
 
 
+
+
 public class Main {
 	
 	public Canvas canvas;
 	
 	public static void main(String[] args) {
 		Main main = new Main();
-//		main.canvas = new Canvas();
-//		
-//		main.canvas.setVisible(true);
 		
-		JFrame frame = new JFrame("Game");
-		frame.setSize(600, 700);	
-		frame.setBackground(new Color(200,100,100));
-		frame.setVisible(true);
+		
 	}
 
 }
