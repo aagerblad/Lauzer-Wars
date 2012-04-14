796bb6bf1a36cc8933e03a755c8d72c1aa0001e7
Dexter Gramfors <dexter.gramfors@gmail.com>
2012-04-15 00:38:42 +0200

    Lade till smooth movement.

--- Lauzer-Wars/src/Main.java
+++ Lauzer-Wars/src/Main.java
@@ -6,9 +6,7 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
-
-
-public class Main extends BasicGame {	
+public class Main extends BasicGame {
 
 	Image land = null;
 	Image player1 = null;
@@ -17,13 +15,15 @@ public class Main extends BasicGame {
 	float player1positionY = 0;
 	float scale = 1.0f;
 	float speed = 100;
+	float distance = 0;
 	boolean key_Apressed = false;
 	boolean key_Wpressed = false;
 	boolean key_Spressed = false;
 	boolean key_Dpressed = false;
-
-
-
+	boolean walkingA = false;
+	boolean walkingD = false;
+	boolean walkingW = false;
+	boolean walkingS = false;
 
 	public Main() {
 		super("Super awesome game");
@@ -35,7 +35,6 @@ public class Main extends BasicGame {
 
 		app.setDisplayMode(800, 600, false);
 		app.start();
-
 
 	}
 
@@ -57,13 +56,12 @@ public class Main extends BasicGame {
 	public void update(GameContainer gc, int delta) throws SlickException {
 		Input input = gc.getInput();
 
-		if(input.isKeyDown(Input.KEY_A))
-		{
-			//            plane.rotate(-0.2f * delta);
+		if (input.isKeyDown(Input.KEY_A)) {
+			// plane.rotate(-0.2f * delta);
 			if (player1positionX > 0 && !key_Apressed) {
 				key_Apressed = true;
-				if (player1.getRotation()==90) {
-					player1positionX-= speed;
+				if (player1.getRotation() == 90) {
+					walkingA = true;
 				} else {
 					player1.setRotation(90);
 				}
@@ -72,46 +70,70 @@ public class Main extends BasicGame {
 			key_Apressed = false;
 		}
 
-		if(input.isKeyDown(Input.KEY_D))
-		{
-			//            plane.rotate(0.2f * delta);
+		if (walkingA == true) {
+			if (distance == speed) {
+				walkingA = false;
+				distance = 0;
+			} else {
+				player1positionX--;
+				distance += 1;
+			}
+		}
+
+		if (input.isKeyDown(Input.KEY_D)) {
+			// plane.rotate(0.2f * delta);
 			if (player1positionX < 800 - player1.getWidth() && !key_Dpressed) {
 				key_Dpressed = true;
-				if (player1.getRotation()==270) {
-					player1positionX+= speed;
+				if (player1.getRotation() == 270) {
+					walkingD = true;
 				} else {
 					player1.setRotation(270);
 				}
-			} 
+			}
 		} else {
 			key_Dpressed = false;
 		}
+		if (walkingD == true) {
+			if (distance == speed) {
+				walkingD = false;
+				distance = 0;
+			} else {
+				player1positionX++;
+				distance += 1;
+			}
+		}
 
-		if(input.isKeyDown(Input.KEY_W))
-		{
-		
+		if (input.isKeyDown(Input.KEY_W)) {
+
 			if (player1positionY > 0 && !key_Wpressed) {
 				key_Wpressed = true;
-				if (player1.getRotation()==0) {				
-					
-					player1positionY-= speed;
+				if (player1.getRotation() == 0) {
+					walkingW = true;
 				} else {
-					
+
 					player1.setRotation(0);
-					
+
 				}
 
 			}
 		} else {
 			key_Wpressed = false;
 		}
+		if (walkingW == true) {
+			if (distance == speed) {
+				walkingW = false;
+				distance = 0;
+			} else {
+				player1positionY--;
+				distance += 1;
+			}
+		}
 
 		if (input.isKeyDown(Input.KEY_S)) {
 			if (player1positionY < (600 - player1.getHeight()) && !key_Spressed) {
 				key_Spressed = true;
-				if (player1.getRotation()==180) {
-					player1positionY+= speed;
-					
+				if (player1.getRotation() == 180) {
+					walkingS = true;
 				} else {
 					player1.setRotation(180);
 				}
@@ -119,16 +141,25 @@ public class Main extends BasicGame {
 		} else {
 			key_Spressed = false;
 		}
-
-		if(input.isKeyDown(Input.KEY_2))
-		{
-			scale += (scale >= 5.0f) ? 0 : 0.1f;
-			player1.setCenterOfRotation(player1.getWidth()/2.0f*scale, player1.getHeight()/2.0f*scale);
+		if (walkingS == true) {
+			if (distance == speed) {
+				walkingS = false;
+				distance = 0;
+			} else {
+				player1positionY++;
+				distance += 1;
+			}
 		}
-		if(input.isKeyDown(Input.KEY_1))
-		{
+
+		if (input.isKeyDown(Input.KEY_2)) {
+			scale += (scale >= 5.0f) ? 0 : 0.1f;
+			player1.setCenterOfRotation(player1.getWidth() / 2.0f * scale,
+					player1.getHeight() / 2.0f * scale);
+		}
+		if (input.isKeyDown(Input.KEY_1)) {
 			scale -= (scale <= 1.0f) ? 0 : 0.1f;
-			player1.setCenterOfRotation(player1.getWidth()/2.0f*scale, player1.getHeight()/2.0f*scale);
+			player1.setCenterOfRotation(player1.getWidth() / 2.0f * scale,
+					player1.getHeight() / 2.0f * scale);
 		}
 
 	}
